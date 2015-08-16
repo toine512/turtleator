fa7e5607ba08a2110d947f3a9a548a93eaa5fb5e
Antoine <toine512@gmail.com>
2015-08-16 16:56:19 +0200

    Tidying

--- discover.php
+++ discover.php
@@ -63,15 +63,15 @@ foreach(array_chunk(array_keys($check), 50, true) as $chunk) //API limits videos
 
 <html lang="en">
 <head>
-		<meta charset="UTF-8">
-		<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
-		<title>toine512 bookmark list</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="icon" href="_img/mineturtle.png" sizes="32x32" type="image/png" />
-		<!--[if lt IE 9]>
-		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
-		<link rel="stylesheet" href="_css/styles.css" media="all" />
+	<meta charset="UTF-8">
+	<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+	<title>toine512 bookmark list</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="_img/mineturtle.png" sizes="32x32" type="image/png" />
+	<!--[if lt IE 9]>
+	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" href="_css/styles.css" media="all" />
 </head>
 <body>
 	<section>
@@ -121,75 +121,36 @@ endforeach;
 ?>
 		</div>
 	</section>
-<?php /*	
-	<table>
-		<caption>∞ table of happiness:</caption>
-		<tr>
-			<th>http://turtle.toine512.fr/<br />http://v.toine512.fr/</th>
-			<th>What is it?</th>
-			<th>Looping behaviour:</th>
-			<th>Start (sec)</th>
-			<th>End (sec)</th>
-			<th>GO!</th>
-		</tr>
-<?php
-foreach($bookmarks as $key => $value)
-{
-	echo "\t\t<tr>\n\t\t\t<td><pre>" . htmlspecialchars($key) . "</pre></td>\n\t\t\t<td>" . htmlspecialchars($value['title']) . "</td>\n";
-	if(array_key_exists('loop', $value))
-	{
-		echo "\t\t\t<td>Loops.</td>\n";
+	<section>
+		<h3>Now with ∞ loop of pain!</h3>
+		<table>
+			<caption>Manual parameters:</caption>
+			<tr>
+				<th>Action</th>
+				<th colspan="2">Parameter</th>
+			</tr>
+			<tr>
+				<td>Always loop</td>
+				<td><pre>?l</pre></td>
+				<td><pre>?l=&lt;anything&gt;</pre></td>
+			</tr>
+			<tr>
+				<td>Never loop</td>
+				<td><pre>?l=0</pre></td>
+				<td><pre>?l=false</pre></td>
+			</tr>
+		</table>
+		<h3>Re-enabling player controls.</h3>
+		<p>Append "c" parameter to the URL.</p>
+		<p>Examples:</p>
+		<pre>http://turtle.toine512.fr/yuki?c</pre>
+		<pre>http://v.toine512.fr/yuki?c</pre>
+		<pre>http://v.toine512.fr/yuki?l=0&amp;c</pre>
+		<h3>Player keyboard controls</h3>
+		<p>Spacebar: Play / Pause<br />Arrow Left: Jump back 10% in the current video<br />Arrow Right: Jump ahead 10% in the current video<br />Arrow Up: Volume up<br />Arrow Down: Volume Down</p>
+	</section>
 	}
-	else
-	{
-		echo "\t\t\t<td>Feels alone.</td>\n";
 	}
-	if(array_key_exists('start', $value))
-	{
-		echo "\t\t\t<td>" . $value['start'] . "</td>\n";
-	}
-	else
-	{
-		echo "\t\t\t<td>∞</td>\n";
-	}
-	if(array_key_exists('end', $value))
-	{
-		echo "\t\t\t<td>" . $value['end'] . "</td>\n";
-	}
-	else
-	{
-		echo "\t\t\t<td>∞</td>\n";
-	}
-	echo "\t\t\t<td><a href=\"http://v.toine512.fr/" . htmlspecialchars($key) . '">http://v.toine512.fr/' . htmlspecialchars($key) . "</a></td>\n\t\t</tr>\n";
-}
-?>
-	</table> */?>
-	<h3>Now with ∞ loop of pain!</h3>
-	<table>
-		<caption>Manual parameters:</caption>
-		<tr>
-			<th>Action</th>
-			<th colspan="2">Parameter</th>
-		</tr>
-		<tr>
-			<td>Always loop</td>
-			<td><pre>?l</pre></td>
-			<td><pre>?l=&lt;anything&gt;</pre></td>
-		</tr>
-		<tr>
-			<td>Never loop</td>
-			<td><pre>?l=0</pre></td>
-			<td><pre>?l=false</pre></td>
-		</tr>
-	</table>
-	<h3>Re-enabling player controls.</h3>
-	<p>Append "c" parameter to the URL.</p>
-	<p>Examples:</p>
-	<pre>http://turtle.toine512.fr/yuki?c</pre>
-	<pre>http://v.toine512.fr/yuki?c</pre>
-	<pre>http://v.toine512.fr/yuki?l=0&amp;c</pre>
-	<h3>Player keyboard controls</h3>
-	<p>Spacebar: Play / Pause<br />Arrow Left: Jump back 10% in the current video<br />Arrow Right: Jump ahead 10% in the current video<br />Arrow Up: Volume up<br />Arrow Down: Volume Down</p>
 </body>
 <!-- Powered by GLaDOS. -->
 <?php
